a1fe9daa4ca8c2ab972a2aa7b47b0c4782698b75
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-14 17:17:06 -0700

    finish daylistitem and style

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayListItem = classNames("day-list__item", {
+    "day-list__item--selected": props.selected === true,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  const formatSpots = () => {
+    let result = props.spots;
+    if (props.spots === 1) {
+      result += " spot";
+    } else if (props.spots === 0) {
+      result = "no spots";
+    } else {
+      result += " spots";
+    }
+    return result;
+  };
+
+  return (
+    <li
+      onClick={() => {
+        props.setDay(props.name);
+      }}
+      className={dayListItem}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()} remaining</h3>
+    </li>
+  );
+}
