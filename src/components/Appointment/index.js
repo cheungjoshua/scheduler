c159194bd562c0526514c0f335c6782222bb1650
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-15 21:51:06 -0700

    finish iterat data over appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function index(props) {
-  return <article className="appointment"></article>;
+  console.log(props.interview);
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
