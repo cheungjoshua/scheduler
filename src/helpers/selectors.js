11513cdb8d1fb60c15e2c66d3afd31ab6bf8860b
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-16 20:32:49 -0700

    selectors code clean up

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,12 +3,16 @@ export function getAppointmentsForDay(state, day) {
   if (state.days.length === 0) {
     return [];
   }
-  // Get match day and make it a array
+
+  // Get match day and make it a obj
   const dayObj = state.days.find((ii) => ii.name === day);
+
   // Check, if get nothing. return []
   if (!dayObj) {
     return [];
   }
+
+  // Find the appointment and make it a array
   const appointmentArray = dayObj.appointments.map((id) => {
     return state.appointments[id];
   });
