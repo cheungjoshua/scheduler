404aae4465254d61aad39c0c6f88600fcf789ddd
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-17 13:06:17 -0700

    implement getInterview function in main app

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment/";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -32,7 +32,15 @@ export default function Application(props) {
   }, []);
 
   const appointmentArray = dailyAppointments.map((appointment) => {
-    return <Appointment key={appointment.id} {...appointment} />;
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
   });
 
   return (
